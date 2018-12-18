4536b47cd04657a301eb65fe88a791b3bf01e17e
Stratege <213346@supinfo.com>
2018-12-18 10:32:21 +0100

    Implementation des interfaces

---
+++ AppSimple/IPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSimple
+{
+	interface IPerson
+	{
+		Person add(Person person);
+		Person Edit(Person person);
+		void Delete(Person person);
+	}
+}
